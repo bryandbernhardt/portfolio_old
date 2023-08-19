e9d0d93ba24768259f9c2b57500d14dcce5fe759
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-08-18 21:00:51 -0300

    feat(Components/Experience): Add experience page

--- src/pages/DesktopView.vue
+++ src/pages/DesktopView.vue
@@ -2,18 +2,21 @@
   <MainBanner />
   <AboutMeView />
   <ProjectsView />
+  <ExperienceView />
 </template>
 
 <script>
 import MainBanner from '../components/MainBanner.vue';
 import AboutMeView from './AboutMeView.vue';
 import ProjectsView from './ProjectsView.vue';
+import ExperienceView from './ExperienceView.vue';
 
 export default {
   components: {
     MainBanner,
     AboutMeView,
     ProjectsView,
+    ExperienceView,
   },
 };
 </script>
---
+++ src/pages/ExperienceView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="experience-container">
+    <h2>Experience</h2>
+
+    <div class="experiences">
+
+      <div class="experience">
+        <h4>DEPLOYMENT ANALYST</h4>
+        <p class="subtitle">IBR SISTEMAS</p>
+        <p>System installation, tax coupon printers,
+          digital certificates, tax configuration for issuing
+          tax documents and training for customers.
+        </p>
+      </div>
+
+      <div class="experience">
+        <h4>FREELANCER FULL STACK DEVELOPER</h4>
+        <p class="subtitle">INCEPTION SURVEY APP</p>
+        <p>
+          Web system for managing products and research for a drug company.
+        </p>
+      </div>
+
+      <div class="experience">
+        <h4>FULL STACK DEVELOPER</h4>
+        <p class="subtitle">MINHA VISISTA</p>
+        <p>
+          Web and mobile system for team management and external sales.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#experience-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  width: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 2rem;
+  gap: 2rem;
+
+  .experiences {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+  }
+}
+
+@media (orientation: portrait) or (max-width: 760px) {
+  #experience-container {
+    .experiences {
+      grid-template-columns: none;
+      grid-template-rows: repeat(3, 1fr);
+      gap: 5rem;
+    }
+  }
+}
+</style>
