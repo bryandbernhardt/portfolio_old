344a7216f6e58ab089872d3a3e779a7208b7f422
Bryan Dietrich Bernhardt <bryan@minhavisita.com.br>
2023-09-07 17:19:48 -0300

    chore(App): remove unecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <DesktopView />
-  <!-- <DesktopView v-if="!isMobile"/>
-  <MobileView v-else /> -->
 </template>
 
 <script>
-import { isMobile } from 'mobile-device-detect';
 import DesktopView from './pages/DesktopView.vue';
-// import MobileView from './pages/MobileView.vue';
 
 export default {
-  data() {
-    return {
-      isMobile,
-    };
-  },
-
   components: {
     DesktopView,
-    // MobileView,
   },
 };
 </script>
